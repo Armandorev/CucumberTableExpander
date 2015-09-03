f8f740545a3ab90b3b60594507f8b2fdbf84d9ee
Armando Sánchez Medina <armandosanchezmedina@gmail.com>
2015-09-03 23:06:13 +0100

    Add Junit and structure

---
+++ src/net/sinoriell/test/java/CucumberTableExpanderTest.java
@@ -0,0 +1,14 @@
+package net.sinoriell.test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CucumberTableExpanderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
